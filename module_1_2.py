f5984fbddd442e267ccc6df136c8961b4c28f6c3
dimasha99 <dimasha2604@mail.ru>
2024-08-01 13:37:59 +0700

    Add files via upload

---
+++ module_1_2.py
@@ -0,0 +1,5 @@
+first_kurs = 'Python'
+chislo_zadac = 12
+chasi = 1.5
+time_of_zadanie = "0,125 часа"
+print("Курс:",first_kurs,',всего задач:',chislo_zadac,',затрачено часов:', chasi, ',среднее время выполнения ', time_of_zadanie,".")
