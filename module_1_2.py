b2adc47ec3898eae4c2454e5380e4a64b915cc81
dimasha99 <dimasha2604@mail.ru>
2024-08-01 18:02:05 +0700

    Update module_1_2.py

--- module_1_2.py
+++ module_1_2.py
@@ -1,5 +1,5 @@
 first_kurs = 'Python'
 chislo_zadac = 12
 chasi = 1.5
-time_of_zadanie = "0,125 часа"
-print("Курс:",first_kurs,',всего задач:',chislo_zadac,',затрачено часов:', chasi, ',среднее время выполнения ', time_of_zadanie,".")
+time_of_zadanie = chasi / chislo_zadac
+print("Курс:",first_kurs,',всего задач:',chislo_zadac,',затрачено часов:', chasi, ',среднее время выполнения ', time_of_zadanie,"часа.")
