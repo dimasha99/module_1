dbfadcc4888394f4a52787d2c328ae8bb3c2c254
dimasha99 <dimasha2604@mail.ru>
2024-08-02 17:49:05 +0700

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var = (1, 2, 3, True, "Football")
+print(immutable_var)
+#immutable_var[4]= "Tennis"
+print(immutable_var)
+mutable_list = [1, 2, 3, True, "Football"]
+print(mutable_list)
+mutable_list[0] = 5
+mutable_list[-1] = "Tennis"
+print(mutable_list)
